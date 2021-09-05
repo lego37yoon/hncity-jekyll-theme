e4a052cbf00309621a7b2a791ea4f858610818ea
윤정민(종이상자) <lego37yoon@outlook.com>
2021-09-05 23:34:59 +0900

    fix bugs and closes enhancement issues
    - closes #13 - too much !important flags
    - closes #12 - :active color is not accent color, "default Carbon Design System" color at mobile
    - fix #11 - Markdown: list UI not working correctly

---
+++ main.js
@@ -0,0 +1,22 @@
+let unordered_list = document.querySelectorAll("#main-content ul:not([class])");
+let ordered_list = document.querySelectorAll("#main-content ol:not([class])");
+
+let unordered_list_item;
+let ordered_list_item;
+let i, j;
+
+for (i = 0; i < unordered_list.length; i++) {
+    unordered_list[i].setAttribute("class", "bx--list--unordered");
+    unordered_list_item = unordered_list[i].querySelectorAll("li");
+    for (j = 0; j < unordered_list_item.length; j++) {
+        unordered_list_item[j].setAttribute("class", "bx--list__item");
+    }
+}
+
+for (i = 0; i < ordered_list.length; i++) {
+    ordered_list[i].setAttribute("class", "bx--list--ordered");
+    ordered_list_item = ordered_list[i].querySelectorAll("li");
+    for (j = 0; j < ordered_list_item.length; j++) {
+        ordered_list_item[j].setAttribute("class", "bx--list__item");
+    }
+}
