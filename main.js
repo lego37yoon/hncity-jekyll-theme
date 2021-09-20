eddf71ac20d73d2b199ec6df915aa50eecdb1e8b
윤정민(종이상자) <lego37yoon@outlook.com>
2021-09-21 00:17:12 +0900

    Partially support table, button
    - Table support : partially - default theme only
    - Button support : partially - default, secondary, tertiary, danger, ghost, danger with ghost
    - Improve excerpt : restrict 10 words to express article, add '…' to show more contents on the article. Closes #24

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ let unordered_list = document.querySelectorAll("#main-content ul:not([class])");
 let ordered_list = document.querySelectorAll("#main-content ol:not([class])");
 let nested_list = document.querySelectorAll("#main-content ul:not([class]) ul, #main-content ol:not([class]) ol");
 
+let table = document.querySelectorAll("table:not([class])");
+let th = document.querySelectorAll("th:not([class])");
+
 let unordered_list_item;
 let ordered_list_item;
 let i, j;
+let temp_th;
 
 for (i = 0; i < unordered_list.length; i++) {
     unordered_list[i].setAttribute("class", "bx--list--unordered");
@@ -24,4 +28,13 @@ for (i = 0; i < ordered_list.length; i++) {
 
 for (i = 0; i < nested_list.length; i++) {
     nested_list[i].classList.add("bx--list--nested");
+}
+
+for (i = 0; i < table.length; i++) {
+    table[i].setAttribute("class", "bx--data-table");
+}
+
+for (i = 0; i < th.length; i++) {
+    temp_th = th[i].innerHTML;
+    th[i].innerHTML = "<span class='bx--table-header-label'>" + temp_th + "</span>";
 }
